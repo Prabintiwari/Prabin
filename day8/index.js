cd6f1a0b765cdb062d601f521cdb45c6842c2522
unknown <prabintiwari964@gmail.com>
2024-05-30 11:18:34 +0200

    Day8

---
+++ day8/index.js
@@ -0,0 +1,64 @@
+console.log("Prabin Tiwari");
+// alert("This is alert");
+document.write("Prabin <br>");
+var a="Prabin ";
+b="Tiwari";
+console.log(a + b);
+
+// const c=20;
+// c=100
+// console.log(c);
+
+// if (a=="Tiwari") 
+//     {
+//         console.log("Prabin");    
+// }
+// else{
+//     console.log("false");
+// }
+//  var d=69
+// if (d>=18 && d<=60) {
+//     document.write("You can vote.");
+    
+// }
+// else if(d>60){
+//     document.write("Sorry you are above 60")
+
+// }
+// // else{
+// //     document.write("Sorry you can't vote");
+// // }
+
+
+// var age =parseInt(prompt("Enter your age"))
+
+// if(!age)
+//     {
+//         document.write("Not valid");
+//     }
+//     else if(age>=18 && age<=60)
+//         {
+//           document.write("You can vote.");
+//         }
+        
+//         else{
+//             document.write("Sorry you can't vote");
+//         }
+// var age =parseInt(prompt("Enter your age"));
+// switch (true) {
+//     case (age > 60):
+//         document.write("Sorry you are above 60")
+        
+//         break;
+//         case (age>=18 && age <=60):
+//             document.write("You can vote.");
+//             break;
+//     default:
+//         document.write("Sorry age not defined!");
+//         break;
+// }
+ var a =10;
+for(i=0;i<5;i++){
+    console.log(i);
+}
+    
